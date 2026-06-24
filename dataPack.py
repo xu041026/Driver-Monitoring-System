5dbb4e0ef74af18eec4e302344fb3c544f6b3680
xu041026 <149317120+xu041026@users.noreply.github.com>
2026-06-24 20:06:24 +0800

    Add files via upload

---
+++ dataPack.py
@@ -0,0 +1,14 @@
+class driverData:
+    def __init__(self):
+        self.dstFrame = None
+        self.srcFrame = None
+        self.isNodding = False
+        self.isBlinking = False
+        self.isYawning = False
+        self.isFreeing = False
+        self.isSleeping = False
+        self.timeStamp = None
+        self.isPlayPhone = False
+        self.isSmoking = False
+        self.isDrinking = False
+        self.isShaking = False
